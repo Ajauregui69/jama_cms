341d851e1b36feaf55d3a5ad683bd8ed514836a4
Baruch001 <baruch_001@outlook.es>
2023-04-17 10:38:32 -0600

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,7 +3,7 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const { host, port, database, user, password } = parse(env("postgres://tkaemioinnuzbd:b89fd74c4b60e24cbd00850ac61bea88a2b955971529fc79b01f91bb13d6d1c1@ec2-34-236-103-63.compute-1.amazonaws.com:5432/d8om5u1n9sb37q"));
   
   return {
     connection: {
