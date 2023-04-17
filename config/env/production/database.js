b7d9748bee5f41aa47da3672273c1aaaa202cfb1
Baruch001 <baruch_001@outlook.es>
2023-04-17 11:53:06 -0600

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,18 @@
-// path: ./config/env/production/database.js
-
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-  
-  return {
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
-      debug: false,
     },
-  }
-};
+    debug: false,
+  },
+});
