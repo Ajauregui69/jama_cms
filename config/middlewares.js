e1eb67ac5a4ba1883edbb999edee6e8c7380427e
jose ivan rodriguez rios <kurivan160901@gmail.com>
2023-06-07 11:16:18 -0600

    Integración cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,4 +9,24 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
